f35b212a4c4872ce7f5896450d6b5b8e0d7f7976
Luiz Gustavo Peres Serpa <luizgserpa@gmail.com>
2022-05-23 21:24:11 -0300

    Adicionado Subnets

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "vpc_cidr_block"{
     type = string
     default = "10.0.0.0/16"
 }
+
+variable "subnet_public_cidr_block" {
+    type = string
+    default = "10.0.1.0/24"
+}
+
+variable "subnet_private_cidr_block" {
+    type = string
+    default = "10.0.2.0/24"
+}

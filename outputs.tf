7eb714e12c4939eeaecf2aa80cdcfae988bfacf2
Luiz Gustavo Peres Serpa <luizgserpa@gmail.com>
2022-05-25 20:23:50 -0300

    Demo GDG

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "ec2" {
   value = {
-      arn = aws_instance.web.arn
       public_ip  = aws_instance.web.public_ip
       private_ip = aws_instance.web.private_ip
       instance_type  = aws_instance.web.instance_type

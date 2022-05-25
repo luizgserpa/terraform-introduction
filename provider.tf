b6ecac5e924fc3d6fe58c315b684e4be71a47fd7
Luiz Gustavo Peres Serpa <luizgserpa@gmail.com>
2022-05-25 16:49:33 -0300

    Alterada Versão Providers

--- provider.tf
+++ provider.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = ">= 4.15.1"
     }
     tls = {
       source = "hashicorp/tls"
+      version = ">= 3.4.0"
     }
   }
 }

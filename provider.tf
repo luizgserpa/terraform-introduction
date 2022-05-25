3e18c0c15c026bd0972c1949a1bbb18e8e2a09b0
Luiz Gustavo Peres Serpa <luizgserpa@gmail.com>
2022-05-25 17:21:26 -0300

    Adicionado Suporte Subnet AZ

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 4.15.1"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = ">= 3.4.0"
     }
   }
--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "main_public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
   tags = {
     "Name" = "Public Subnet"
@@ -9,8 +10,9 @@ resource "aws_subnet" "main_public" {
 }
 
 resource "aws_subnet" "main_private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     "Name" = "Private Subnet"
